c19235c0d993932291cde46676dfd936577cee88
Alisss <alisssing555@gmail.com>
2024-02-07 16:23:05 +0300

    add models from BD

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 require('dotenv').config()
 const sequelize = require('./db')
+const models = require('./models/models.js')
+const cors = require('cors')
 const express = require('express') // вызываем модуль
 
 const PORT = process.env.PORT || 5000
 
 const app = express() // создаем объект путем выхова функции экспресс
+app.use(cors())
+app.use(express.json())// для парсинга json формата
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "работает!!!" })
+})
 
 const start = async () => {
   try {
