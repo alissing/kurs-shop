84153ff5a5e2c7deeeb3c0e3d9092478f02ddb4f
Alisss <alisssing555@gmail.com>
2024-02-10 00:57:28 +0300

    add routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 require('dotenv').config()
+
 const sequelize = require('./db')
 const models = require('./models/models.js')
 const cors = require('cors')
 const express = require('express') // вызываем модуль
+const router = require('./routes/index')
 
 const PORT = process.env.PORT || 5000
 
 const app = express() // создаем объект путем выхова функции экспресс
+
 app.use(cors())
 app.use(express.json())// для парсинга json формата
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: "работает!!!" })
-})
+app.use('/api', router)
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: "работает!!!" })
+// })
 
 const start = async () => {
   try {
